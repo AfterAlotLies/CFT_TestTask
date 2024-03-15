b3efb2d6b87cee7024e3ee6d29a789ecb4ad0923
Vyacheslav <guzxaroh@gmail.com>
2024-03-15 21:12:29 +0700

    added localizable string

---
+++ CFT_TestTask/Resources/LocalizedStrings/LocalizableStrings.swift
@@ -0,0 +1,12 @@
+//
+//  LocalizableStrings.swift
+//  CFT_TestTask
+//
+//  Created by Vyacheslav on 15.03.2024.
+//
+
+import Foundation
+
+public enum LocalizableStrings {
+    static let navigationTitle = NSLocalizedString("NoteListViewController.navigationTitle", comment: "")
+}
